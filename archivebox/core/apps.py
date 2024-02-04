595b569fb2705418db505a1fb6f07e8fe1c59f1e
Milan Hauth <milahu@gmail.com>
2024-02-04 09:31:00 +0100

    fix: sqlite3.OperationalError: no such column: django_migrations.id

--- archivebox/core/apps.py
+++ archivebox/core/apps.py
@@ -3,5 +3,3 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     name = 'core'
-    # WIP: broken by Django 3.1.2 -> 4.0 migration
-    default_auto_field = 'django.db.models.UUIDAutoField'
